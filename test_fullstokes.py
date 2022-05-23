71b41e25e0231a51c3108ef1ba2c26d0c31d2502
Jaren Ashcraft <jashcraft@email.arizona.edu>
2022-05-23 10:54:42 -0700

    Update with Full Stokes test

--- test_fullstokes.py
+++ test_fullstokes.py
@@ -3,7 +3,7 @@ import mueller as mul
 
 Sin = mul.StokesRandom()
 
-Sout = FullStokesPolarimeterMeasurement(Sin,11)
+Sout = FullStokesPolarimeterMeasurement(Sin,101)
 
 print('Stokes In')
 print(Sin)
@@ -11,6 +11,6 @@ print('Stokes Measured')
 print(Sout)
 
 Sin_array = GenerateStokesArray(Sin,51)
-Sout_array = FullStokesPolarimeter(Sin_array,11)
+Sout_array = FullStokesPolarimeter(Sin_array,101)
 
 PlotStokesArray(Sout_array,Sin=Sin_array)
