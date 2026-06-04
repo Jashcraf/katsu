a5b68c08f281e5bc5191938b098ea9e6a9b4c4ab
Jashcraf <jarenashcraft@ucsb.edu>
2026-06-03 18:08:35 -1000

    progress in a Zodiaxy Katsu, still need to resolve how to push updates to the forward model. Probably something something pytrees or x.set(other_things)

--- katsu/models.py
+++ katsu/models.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from typing import Any
 
 import zodiax as zdx
@@ -8,18 +6,54 @@ from katsu.katsu_math import np
 from katsu.mueller import linear_diattenuator, linear_retarder
 
 
+def _digest_variables(variable, params):
+    """
+    Digest the variable specification into a dictionary of parameters to vary.
+
+    Parameters
+    ----------
+    variable : bool, list, or dict
+        The variable specification.
+    params : list
+        The list of parameters to vary.
+
+    Returns
+    -------
+    dict
+        A dictionary of parameters to vary, with parameter names as keys and
+        boolean values indicating whether to vary the parameter.
+    """
+    if isinstance(variable, bool):
+        return {p: variable for p in params}
+    elif isinstance(variable, list):
+        return {p: True for p in params if p in variable}
+    elif isinstance(variable, dict):
+        return variable
+    else:
+        raise ValueError(f"Invalid variable type: {type(variable)}")
+
+
 class MuellerMatrix(zdx.Base):
     """Base class for all Mueller matrix elements.
 
     Can also be used to wrap an arbitrary 4x4 Mueller matrix. Provides support
     for Python's matrix multiplication operator (`@`) to allow composing
     multiple optical elements into a single composite `MuellerMatrix`.
+
+    Generally variable elements should be specified as a boolean, list, or dict.
+    boolean: True means vary all free parameters, False means fix all free parameters
+    dict: dict of attributes to vary and explicitly specify if they are variable
+
+    internally, these are stored as a dictionary
     """
 
     matrix: Any
-    n_params: int
+    variable: bool or dict
+    shape: tuple or None
+    _n_params: int
+    _trainable: dict
 
-    def __init__(self, matrix: Any, n_params: int = 0):
+    def __init__(self, matrix, variable=False, shape=None):
         """
         Parameters
         ----------
@@ -29,31 +63,27 @@ class MuellerMatrix(zdx.Base):
             The number of optimizeable parameters. Defaults to 0.
         """
         self.matrix = matrix
-        self.n_params = n_params
-
-    def matrix_from_params(self, p):
-        """Returns the matrix computed from a vector of parameters.
-
-        This is used by the `Interface` class during forward modeling. Fixed
-        optics return their stored matrix, while variable optics override this
-        method to recompute the matrix from the parameters `p`.
-        """
-        return self.matrix
+        self.variable = variable
+        self.shape = shape
+        self._n_params = 0
+        self._trainable = {}
 
     def __matmul__(self, other):
         if hasattr(other, "matrix"):
             return MuellerMatrix(self.matrix @ other.matrix)
 
         # Handle jax arrays, numpy arrays, or other array-likes safely
-        elif hasattr(other, "shape") and len(getattr(other, "shape", ())) == 2:
-            return MuellerMatrix(self.matrix @ other)
+        # This will return a raw array, not a `MuellerMatrix`, so the type
+        # of the array to the right is maintained
+        elif hasattr(other, "shape") and len(getattr(other, "shape", ())) >= 1:
+            return self.matrix @ other
 
         else:
             return NotImplemented, "Unsupported operand type: " + str(type(other))
 
     def __rmatmul__(self, other):
-        if hasattr(other, "shape") and len(getattr(other, "shape", ())) == 2:
-            return MuellerMatrix(other @ self.matrix)
+        if hasattr(other, "shape") and len(getattr(other, "shape", ())) >= 1:
+            return other @ self.matrix
         else:
             return NotImplemented
 
@@ -83,44 +113,81 @@ class LinearRetarder(MuellerMatrix):
     """
 
     # Typing required by zodiax/equinox
-    _fast_axis: float
-    _retardance: float
+    fast_axis: float or np.ndarray
+    retardance: float or np.ndarray
 
-    def __init__(self, fast_axis, retardance, variable=False):
-        self._fast_axis = float(fast_axis)
-        self._retardance = float(retardance)
-        self.n_params = 2 if variable else 0
-        self.matrix = linear_retarder(self._fast_axis, self._retardance)
+    def __init__(self, fast_axis, retardance, variable=False, shape=None):
 
-    @property
-    def params(self):
-        return (self._fast_axis, self._retardance)
+        # These can be floats or ndarrays because katsu accounts for the difference in shape
+        self.fast_axis = fast_axis
+        self.retardance = retardance
+
+        # From MuellerMatrix
+        self.matrix = linear_retarder(self.fast_axis, self.retardance, shape=shape)
+        self.shape = shape
+
+        # Digest trainable parameters
+        self.variable = variable
+        self._trainable = _digest_variables(self.variable, ["fast_axis", "retardance"])
+        self._n_params = sum(self._trainable.values())
 
     @classmethod
-    def as_quarter_wave_plate(cls, fast_axis, variable=False):
-        """Quarter-wave plate (retardance = π/2)."""
-        return cls(fast_axis, np.pi / 2, variable=variable)
+    def as_quarter_wave_plate(cls, fast_axis, variable=False, shape=None):
+        """Quarter-wave plate (retardance = π/2). This also sets retardance to π/2, and
+        does not permit Tmin to be varied."""
+        if not isinstance(variable, bool):
+            if variable["retardance"]:
+                raise ValueError(
+                    "Retardance cannot be varied in a quarter-wave plate model"
+                )
+        else:
+            variable = {"fast_axis": variable}
+
+        return cls(
+            fast_axis=fast_axis, retardance=np.pi / 2, variable=variable, shape=shape
+        )
 
     @classmethod
-    def as_half_wave_plate(cls, fast_axis, variable=False):
-        """Half-wave plate (retardance = π)."""
-        return cls(fast_axis, np.pi, variable=variable)
+    def as_half_wave_plate(cls, fast_axis, variable=False, shape=None):
+        """Half-wave plate (retardance = π). This also sets retardance to π, and
+        does not permit Tmin to be varied."""
+        if not isinstance(variable, bool):
+            if variable["retardance"]:
+                raise ValueError(
+                    "Retardance cannot be varied in a half-wave plate model"
+                )
+        else:
+            variable = {"fast_axis": variable}
+
+        return cls(
+            fast_axis=fast_axis, retardance=np.pi, variable=variable, shape=shape
+        )
 
     def update(self, x):
         """
         Update the model parameters and matrix from the given vector `x`.
 
+        This accepts a vector of parameters whose length must match the number
+        of trainable parameters (i.e. 2 for a linear retarder).
+
         Parameters
         ----------
         x : array-like
-            The vector of parameters to update the model with.
+            The vector of parameters to update the model with. This is ordered
+            as fast axis, then retardance.
         """
+        if len(x) != len(self._trainable):
+            raise ValueError(
+                f"Expected {len(self._trainable)} parameters, got {len(x)}"
+            )
 
-        # handle case for fixed diattenuation
-        if self.n_params == 1:
-            self.matrix = linear_retarder(x[0], self._retardance)
-        else:
-            self.matrix = linear_retarder(x[0], x[1])
+        # Update free parameters if trainable
+        for i, param in enumerate(self._trainable):
+            if self._trainable[param]:
+                setattr(self, param, x[i])
+
+        # Update the Mueller matrix
+        self.matrix = linear_retarder(self.fast_axis, self.retardance, shape=self.shape)
         return self.matrix
 
 
@@ -139,26 +206,38 @@ class LinearDiattenuator(MuellerMatrix):
     """
 
     # Typing required by zodiax/equinox
-    _transmission_axis: float
-    _Tmin: float
+    transmission_axis: float
+    Tmin: float
+    _n_params: int
 
-    def __init__(self, transmission_axis, Tmin, variable=False):
-        self._transmission_axis = float(transmission_axis)
-        self._Tmin = float(Tmin)
-        self.n_params = 2 if variable else 0
-        self.matrix = linear_diattenuator(self._transmission_axis, self._Tmin)
+    def __init__(self, transmission_axis, Tmin, variable=False, shape=None):
+        self.transmission_axis = float(transmission_axis)
+        self.Tmin = float(Tmin)
 
-    @property
-    def params(self):
-        return (self._transmission_axis, self._Tmin)
+        # from MuellerMatrix
+        self.matrix = linear_diattenuator(
+            self.transmission_axis, self.Tmin, shape=shape
+        )
+        self.shape = shape
+
+        # Digest trainable parameters
+        self.variable = variable
+        self._trainable = _digest_variables(
+            self.variable, ["transmission_axis", "Tmin"]
+        )
+        self._n_params = sum(self._trainable.values())
 
     @classmethod
-    def as_polarizer(cls, transmission_axis, variable=False):
-        """Ideal linear polarizer (Tmin = 0)."""
-        instance = cls(transmission_axis, 0.0, variable=variable)
-        if variable and instance._Tmin == 0.0:
-            instance.n_params = 1
-        return instance
+    def as_polarizer(cls, transmission_axis, variable=False, shape=None):
+        """Ideal linear polarizer (Tmin = 0). This also sets Tmin to zero, and
+        does not permit Tmin to be varied."""
+        if not isinstance(variable, bool):
+            if variable["Tmin"]:
+                raise ValueError("Tmin cannot be varied in a polarizer model")
+        else:
+            variable = {"transmission_axis": variable}
+
+        return cls(transmission_axis, 0.0, variable=variable, shape=shape)
 
     def update(self, x):
         """
@@ -170,11 +249,15 @@ class LinearDiattenuator(MuellerMatrix):
             The vector of parameters to update the model with.
         """
 
-        # handle case for fixed diattenuation
-        if self.n_params == 1:
-            self.matrix = linear_diattenuator(x[0], 0.0)
-        else:
-            self.matrix = linear_diattenuator(x[0], x[1])
+        # Update free parameters if trainable
+        for i, param in enumerate(self._trainable):
+            if self._trainable[param]:
+                setattr(self, param, x[i])
+
+        # Update the Mueller matrix
+        self.matrix = linear_diattenuator(
+            self.fast_axis, self.retardance, shape=self.shape
+        )
         return self.matrix
 
 
@@ -191,27 +274,51 @@ class Model(zdx.Base):
 
     optics: list[MuellerMatrix]
     data: np.ndarray
-    n_params: int
     offsets: list[tuple[int, int]]
+    system_matrix: np.ndarray
+    _n_params: int
     _fg_func: callable or None
 
     def __init__(self, optics_list, data=None):
         self.optics = optics_list
         self.data = data
-        self.n_params = 0
+        self._n_params = 0
 
         # Calculate offsets for the flat parameter vector x
         self.offsets = []
-        assert any([optic.variable for optic in self.optics]), (
-            "No variable optics found, class initialization failed"
-        )
         cursor = 0
+
+        # Build understanding of slices for parameter vector x
         for optic in self.optics:
-            self.offsets.append((cursor, cursor + optic.n_params))
-            cursor += optic.n_params
-            self.n_params += optic.n_params
+            self.offsets.append((cursor, cursor + optic._n_params))
+            cursor += optic._n_params
+            self._n_params += optic._n_params
 
         self._fg_func = None
+        self.system_matrix = np.eye(4)
+
+    def _update(self, x):
+        """Update the optics and full system matrix
+
+        Parameters
+        ----------
+        x : np.ndarray
+            The vector of all optimizeable parameters.
+
+        """
+        cursor = 0
+        system_matrix = np.eye(4)
+
+        for optic, offsets in zip(self.optics, self.offsets):
+            # Update optic variables
+            if optic._n_params > 0:
+                optic.update(x[cursor : cursor + optic._n_params])
+            cursor += optic._n_params
+
+            # Update the system matrix
+            system_matrix = optic.matrix @ system_matrix
+
+        self.system_matrix = system_matrix
 
     def forward(self, x, stokes=None):
         """Compute the forward model / objective function.
@@ -222,26 +329,18 @@ class Model(zdx.Base):
             The vector of all optimizeable parameters.
         stokes : np.ndarray, optional
             The input Stokes vector. Defaults to [1, 0, 0, 0].
+
         """
+        # Update the optics
+        self._update(x)
+
         if stokes is None:
             stokes = np.array([1.0, 0.0, 0.0, 0.0])
 
-        system_matrix = np.eye(4)
-        for optic, offset in zip(self.optics, self.offsets):
-            if offset is not None:
-                p = x[offset[0] : offset[1]]
-                # Optics are applied in sequence (M_n @ ... @ M_1)
-                system_matrix = optic.matrix_from_params(p) @ system_matrix
-            else:
-                system_matrix = optic.matrix @ system_matrix
-
+        # Multiply by stokes
         final_stokes = system_matrix @ stokes
 
-        if self.data is not None:
-            # Simple MSE loss if data is provided
-            return np.mean((final_stokes - self.data) ** 2)
-
-        return final_stokes[0]  # Return intensity by default
+        return final_stokes[..., 0]  # Return intensity observed
 
     def fg(self, x):
         """Compute both the function value and its gradient.
